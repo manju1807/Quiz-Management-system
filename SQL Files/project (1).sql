0ef597d5af68833684c872e07c0c0e7f4878c5b6
manja <rmanjunath7692@gmail.com>
2023-01-17 11:38:26 +0530

    sql update

--- SQL Files/project (1).sql
+++ SQL Files/project (1).sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 20, 2019 at 04:29 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 17, 2023 at 06:05 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `project`
@@ -24,9 +25,8 @@ DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `leaderboard`()
-    NO SQL
-select q.quizname,s.score,s.totalscore,st.name,s.mail from score s,student st,quiz q where s.mail=st.mail and q.quizid=s.quizid order by score DESC$$
+DROP PROCEDURE IF EXISTS `leaderboard`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `leaderboard` ()  NO SQL select q.quizname,s.score,s.totalscore,st.name,s.mail from score s,student st,quiz q where s.mail=st.mail and q.quizid=s.quizid order by score DESC$$
 
 DELIMITER ;
 
@@ -36,8 +36,9 @@ DELIMITER ;
 -- Table structure for table `dept`
 --
 
+DROP TABLE IF EXISTS `dept`;
 CREATE TABLE IF NOT EXISTS `dept` (
-  `dept_id` int(11) NOT NULL,
+  `dept_id` int NOT NULL,
   `dept_name` varchar(3) DEFAULT NULL,
   PRIMARY KEY (`dept_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -58,13 +59,14 @@ INSERT INTO `dept` (`dept_id`, `dept_name`) VALUES
 -- Table structure for table `questions`
 --
 
+DROP TABLE IF EXISTS `questions`;
 CREATE TABLE IF NOT EXISTS `questions` (
   `qs` varchar(200) NOT NULL,
   `op1` varchar(30) NOT NULL,
   `op2` varchar(30) NOT NULL,
   `op3` varchar(30) NOT NULL,
   `answer` varchar(30) NOT NULL,
-  `quizid` int(11) NOT NULL,
+  `quizid` int NOT NULL,
   UNIQUE KEY `qs` (`qs`),
   KEY `quizid` (`quizid`),
   KEY `quizid_2` (`quizid`),
@@ -95,14 +97,15 @@ INSERT INTO `questions` (`qs`, `op1`, `op2`, `op3`, `answer`, `quizid`) VALUES
 -- Table structure for table `quiz`
 --
 
+DROP TABLE IF EXISTS `quiz`;
 CREATE TABLE IF NOT EXISTS `quiz` (
-  `quizid` int(11) NOT NULL AUTO_INCREMENT,
+  `quizid` int NOT NULL AUTO_INCREMENT,
   `quizname` varchar(20) NOT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `mail` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`quizid`),
   KEY `mail` (`mail`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `quiz`
@@ -117,10 +120,9 @@ INSERT INTO `quiz` (`quizid`, `quizname`, `date_created`, `mail`) VALUES
 -- Triggers `quiz`
 --
 DROP TRIGGER IF EXISTS `ondeleteqs`;
-DELIMITER //
-CREATE TRIGGER `ondeleteqs` AFTER DELETE ON `quiz`
- FOR EACH ROW delete from questions where questions.quizid=old.quizid
-//
+DELIMITER $$
+CREATE TRIGGER `ondeleteqs` AFTER DELETE ON `quiz` FOR EACH ROW delete from questions where questions.quizid=old.quizid
+$$
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -129,17 +131,18 @@ DELIMITER ;
 -- Table structure for table `score`
 --
 
+DROP TABLE IF EXISTS `score`;
 CREATE TABLE IF NOT EXISTS `score` (
-  `slno` int(11) NOT NULL AUTO_INCREMENT,
-  `score` int(11) NOT NULL,
-  `quizid` int(11) NOT NULL,
+  `slno` int NOT NULL AUTO_INCREMENT,
+  `score` int NOT NULL,
+  `quizid` int NOT NULL,
   `mail` varchar(30) DEFAULT NULL,
-  `totalscore` int(11) DEFAULT NULL,
+  `totalscore` int DEFAULT NULL,
   `remark` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`slno`),
   KEY `quizid` (`quizid`),
   KEY `mail` (`mail`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `score`
@@ -147,16 +150,16 @@ CREATE TABLE IF NOT EXISTS `score` (
 
 INSERT INTO `score` (`slno`, `score`, `quizid`, `mail`, `totalscore`, `remark`) VALUES
 (13, 6, 5, 'rakeshmariyaplar1@gmail.com', 6, 'good'),
-(14, 2, 4, 'rakeshmariyaplar1@gmail.com', 3, 'good');
+(14, 2, 4, 'rakeshmariyaplar1@gmail.com', 3, 'good'),
+(15, 3, 4, 'rmanjunath7692@gmail.com', 3, 'good');
 
 --
 -- Triggers `score`
 --
 DROP TRIGGER IF EXISTS `remarks`;
-DELIMITER //
-CREATE TRIGGER `remarks` BEFORE INSERT ON `score`
- FOR EACH ROW set NEW.remark = if(NEW.score = 0, 'bad', 'good')
-//
+DELIMITER $$
+CREATE TRIGGER `remarks` BEFORE INSERT ON `score` FOR EACH ROW set NEW.remark = if(NEW.score = 0, 'bad', 'good')
+$$
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -165,6 +168,7 @@ DELIMITER ;
 -- Table structure for table `staff`
 --
 
+DROP TABLE IF EXISTS `staff`;
 CREATE TABLE IF NOT EXISTS `staff` (
   `staffid` varchar(10) NOT NULL,
   `name` varchar(20) NOT NULL,
@@ -184,6 +188,7 @@ CREATE TABLE IF NOT EXISTS `staff` (
 --
 
 INSERT INTO `staff` (`staffid`, `name`, `mail`, `phno`, `gender`, `DOB`, `pw`, `dept`) VALUES
+('101', 'Anushree', 'anushree9019@gmail.com', '9019247975', 'F', '2002-03-19', 'raNVgEH7z4B6.', 'CSE'),
 ('svit1', 'B G VINAYAK', 'BHATVINAYAK94@GMAIL.COM', '9740834260', 'M', '1999-09-23', 'ral7gku4rfhLk', 'CSE'),
 ('123', 'Rakesh M R', 'rakeshmr723@gmail.com', '9901735897', 'M', '1999-10-07', 'rajJYeVNCiGD2', 'ISE');
 
@@ -193,6 +198,7 @@ INSERT INTO `staff` (`staffid`, `name`, `mail`, `phno`, `gender`, `DOB`, `pw`, `
 -- Table structure for table `student`
 --
 
+DROP TABLE IF EXISTS `student`;
 CREATE TABLE IF NOT EXISTS `student` (
   `usn` varchar(10) NOT NULL,
   `name` varchar(20) NOT NULL,
@@ -217,6 +223,7 @@ INSERT INTO `student` (`usn`, `name`, `mail`, `phno`, `gender`, `DOB`, `pw`, `de
 ('1va17cs010', 'B G VINAYAK', 'BHATVINAYAK94@GMAIL.COM', '9740834260', 'M', '1999-09-23', 'ral7gku4rfhLk', 'CSE'),
 ('1va17cs140', 'Rakesh Mariyaplar', 'rakeshmariyaplar1@gmail.com', '6360300095', 'M', '1999-10-07', 'rajJYeVNCiGD2', 'CSE'),
 ('1va17cs040', 'Rakesh M R', 'rakeshmr723@gmail.com', '9901735897', 'M', '2000-10-07', 'rajJYeVNCiGD2', 'CSE'),
+('1KS20CS054', 'Manjunath R', 'rmanjunath7692@gmail.com', '6363358647', 'M', '2002-07-18', 'rayD.ut57ldEo', 'CSE'),
 ('1va17cs051', 'Siddhanth Sipoliya', 'siddhanthsipoliya@saividya.ac.', '7619360459', 'M', '1999-11-15', 'ray.whoA8HjCQ', 'CSE');
 
 --
@@ -235,6 +242,7 @@ ALTER TABLE `quiz`
 ALTER TABLE `score`
   ADD CONSTRAINT `score_ibfk_1` FOREIGN KEY (`quizid`) REFERENCES `quiz` (`quizid`) ON DELETE CASCADE,
   ADD CONSTRAINT `score_ibfk_2` FOREIGN KEY (`mail`) REFERENCES `student` (`mail`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
